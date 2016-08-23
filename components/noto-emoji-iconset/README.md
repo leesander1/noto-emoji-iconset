dab9dee59d51d64dbffdfd43fd52e103432b72c5
raulsntos <raulsntos@gmail.com>
2016-08-24 01:17:02 +0200

    seed gh-pages

---
+++ components/noto-emoji-iconset/README.md
@@ -0,0 +1,72 @@
+# noto-emoji-iconset
+
+Iconset for [`iron-icon`](https://elements.polymer-project.org/elements/iron-icon) to use Google's Emojis.
+
+[See for yourself](https://raulsntos.github.io/noto-emoji-iconset).
+
+## How to install
+You can clone this repo directly to your server but I recommend using bower:
+
+`bower install --save raulsntos/noto-emoji-iconset`
+
+## Important note
+The emojis will not render properly if you don't use Shadow DOM so be sure to enable it.
+
+**The good news:** Polymer will switch to Shadow DOM by default eventually (soon).
+
+Take a look at the [demo](https://github.com/raulsntos/noto-emoji-iconset/blob/master/demo/index.html) to see how I'm using the iconset. Just be sure to add this code before loading Polymer (or every element implemented with Polymer):
+```html
+<script>
+window.Polymer = {
+  dom: 'shadow'
+}
+</script>
+```
+
+## How to use the set
+To use the set simply import this set and use it like any other iconset. Use the prefix **emoji** followed by colon (**:**) and the emoji in unicode (`🎉`). Example:
+```html
+<html>
+  <head>
+    ...
+    <link rel="import" href="bower_components/noto-emoji-iconset/noto-emoji-iconset.html">
+    ...
+  </head>
+  <body>
+    ...
+      <iron-icon icon="emoji:🎉"></iron-icon>
+    ...
+  </body>
+</html>
+```
+
+## But it's hard to type emojis in my laptop! :angry:
+I know! That's why there's also a Polymer element included `emoji-icon` which lets you use the emoji shortname (like you do in GitHub). Example:
+```html
+<html>
+  <head>
+    ...
+    <link rel="import" href="bower_components/noto-emoji-iconset/emoji-icon.html">
+    ...
+  </head>
+  <body>
+    ...
+      <emoji-icon emoji="tada"></emoji-icon>
+    ...
+  </body>
+</html>
+```
+
+### Nice features of `emoji-icon`
+- You can use shortnames instead of typing the emoji (making it easier to use when you are not developing using your phone :wink:)
+- The element uses a dictionary to translate emoji shortnames to unicode, the dictionary is stored in `emoji.json`. The element loads the JSON file **only once** and waits until the Promise is resolved to load the emoji.
+
+## Can I change the size?
+Yes! If you are using `iron-icon`, see how to do it in their [documentation](https://elements.polymer-project.org/elements/iron-icon#styling). If you are using `emoji-icon` you can also find how in `iron-icon`'s documentation because it's the exactly the same but replacing `iron` with `emoji` in the CSS variables.
+
+## How to build the iconset
+You can build the iconset yourself by using the build.go file included in this repository, simple use `go run build.go`. Note that you need to have `git` installed since the script will clone the Noto GitHub repository (you can also download the repository manually, the script will not clone it as long as a folder named `noto-emoji` exists in the root of this project). If you have already cloned or downloaded the Noto repository but want to update it to the latest version use the flag `-update` when running the script: `go run build.go -update`. The script will replace the `noto-emoji-iconset.html` file.
+
+## Known issues
+- I'm using the SVG icons provided by Google in the [Noto repository](https://github.com/googlei18n/noto-emoji) and they are currently outdated so there are a few missing emojis, when Google updates their repository I'll include the new emoji. If the emoji icon is not found. If you want to know the state of this issue check the Noto repository issue [#62](https://github.com/googlei18n/noto-emoji/issues/62).
+- I would prefer to make `emoji-icon` extend from `iron-icon` instead of creating a new element with an `iron-icon` tag inside and redefining every CSS variable but Polymer does not seem to support extending custom elements yet. See issue [#2280](https://github.com/Polymer/polymer/issues/2280) in the Polymer repo.
---
+++ components/noto-emoji-iconset/build.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+	"text/template"
+)
+
+var notoDirectory = "noto-emoji"
+
+// Emoji contains the data of an emoji
+type Emoji struct {
+	Unicode string
+	SVG     string
+}
+
+func updateFile(update bool, path string) bool {
+	_, err := os.Stat(path)
+	exists := !os.IsNotExist(err)
+	if exists && update {
+		os.RemoveAll(path)
+	}
+	if !exists || update {
+		return true
+	}
+	return false
+}
+
+func updateNotoEmoji(update bool) {
+	if updateFile(update, notoDirectory) {
+		fmt.Println("Downloading noto-emoji...")
+		exec.Command("git", "clone", "https://github.com/googlei18n/noto-emoji", notoDirectory).Run()
+		fmt.Println("noto-emoji finished downloading")
+	}
+}
+
+func unicodeToEmoji(unicode string, separator string) (string, error) {
+	unicodeStr := strings.Split(unicode, separator)
+	for index, unicodeChar := range unicodeStr {
+		hex, err := strconv.ParseInt(unicodeChar, 16, 64)
+		if err != nil {
+			return "", err
+		}
+		unicodeStr[index] = string(hex)
+	}
+	return strings.Join(unicodeStr, ""), nil
+}
+
+func fileToEmoji(filePath string, fileName string) (*Emoji, error) {
+	emoji := &Emoji{}
+
+	file, err := os.Open(filePath + fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	regexpNewLine := regexp.MustCompile(`\r?\n`)
+	regexpTabs := regexp.MustCompile(`\t`)
+
+	regexpHeader := regexp.MustCompile(".*<svg.*?>")
+	regexpFooter := regexp.MustCompile("</svg>")
+
+	code := regexpNewLine.ReplaceAllString(string(bytes), "")
+	code = regexpTabs.ReplaceAllString(code, "")
+	code = regexpHeader.ReplaceAllString(code, "")
+	code = regexpFooter.ReplaceAllString(code, "")
+
+	unicode := fileName[len("emoji_u") : len(fileName)-len(".svg")]
+	unicodedEmoji, err := unicodeToEmoji(unicode, "_")
+	if err != nil {
+		return nil, err
+	}
+	emoji.Unicode = unicodedEmoji
+	emoji.SVG = code
+
+	return emoji, nil
+}
+
+func readEmojis() []Emoji {
+	emojis := []Emoji{}
+
+	path := notoDirectory + "/svg/"
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Parsing ", len(files), " elements...")
+	for index, file := range files {
+		emoji, err := fileToEmoji(path, file.Name())
+		if err != nil {
+			fmt.Println(err.Error())
+			fmt.Println("Error parsing element ", index+1, " of ", len(files))
+			continue
+		}
+		emojis = append(emojis, *emoji)
+		fmt.Println("Parsed element ", index+1, " of ", len(files))
+	}
+
+	return emojis
+}
+
+func writeIconset(data []Emoji) {
+	content := `<!--
+` + "`noto-emoji-iconset`" + ` imports the Google emojis from Noto as an iconset you can use with the ` + "`iron-icon`" + ` element.
+See [iron-icon](https://elements.polymer-project.org/elements/iron-icon) for more information about working with icons.
+You can also use the [` + "`emoji-icon`" + `](#emoji-icon) element to use emojis more conveniently!
+
+### Usage
+
+` + "```html" + `
+<iron-icon icon="emoji:🎉"></iron-icon>
+` + "```" + `
+
+@group Noto Emoji elements
+@pseudoElement noto-emoji-iconset
+@demo demo/index.html
+@homepage https://github.com/raulsntos/noto-emoji-iconset
+-->
+<link rel="import" href="../iron-icon/iron-icon.html">
+<link rel="import" href="../iron-iconset-svg/iron-iconset-svg.html">
+
+<iron-iconset-svg name="emoji" size="128">
+
+<svg><defs>
+{{ range $element := . -}}
+<g id="{{ .Unicode }}">{{ .SVG }}</g>
+{{ end -}}
+</defs></svg>
+</iron-iconset-svg>`
+
+	t := template.New("t")
+	t, err := t.Parse(content)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create("noto-emoji-iconset.html")
+	err = t.Execute(file, data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Emojione represents an emoji in Emojione data
+type Emojione struct {
+	Unicode   string `json:"unicode"`
+	Shortname string `json:"shortname"`
+}
+
+func writeDictionary() {
+	fmt.Println("Writing Emoji Dictionary...")
+	resp, err := http.Get("https://raw.githubusercontent.com/Ranks/emojione/master/emoji.json")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var emojione map[string]Emojione
+	err = json.Unmarshal(data, &emojione)
+	if err != nil {
+		panic(err)
+	}
+
+	emojis := make(map[string]string)
+
+	fmt.Println("Parsing ", len(emojione), " emojis...")
+
+	index := 0
+	for _, emoji := range emojione {
+		unicodedEmoji, err := unicodeToEmoji(emoji.Unicode, "-")
+		if err != nil {
+			fmt.Println(err.Error())
+			fmt.Println("Error parsing element ", index+1, " of ", len(emojione))
+			continue
+		}
+		emojis[emoji.Shortname] = unicodedEmoji
+		fmt.Println("Parsed element ", index+1, " of ", len(emojione))
+		index++
+	}
+
+	fmt.Println("Parsing emojis as JSON...")
+	jsonData, err := json.Marshal(emojis)
+	if err != nil {
+		panic(err)
+	}
+
+	ioutil.WriteFile("emoji.json", jsonData, 0644)
+	fmt.Println("emoji.json file created")
+}
+
+func updateDictionary(update bool) {
+	if updateFile(update, "emoji.json") {
+		writeDictionary()
+	}
+}
+
+func main() {
+	updateNoto := flag.Bool("update-noto", false, "update noto emoji")
+	updateJSON := flag.Bool("update-json", false, "update emoji dictionary")
+
+	flag.Parse()
+
+	updateNotoEmoji(*updateNoto)
+	emojis := readEmojis()
+	writeIconset(emojis)
+	updateDictionary(*updateJSON)
+}
